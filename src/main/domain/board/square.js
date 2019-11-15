9ae90bfb916309f6a21a01bc0fb989e3cb30fb27
AniaDM <anne@dali-maryoush.eu>
2019-11-15 19:26:25 +0100

    Tests for Queen

--- src/main/domain/board/square.js
+++ src/main/domain/board/square.js
@@ -26,6 +26,14 @@ export class Square {
             row = 7
         }else if(algebricRow === 2){
             row = 6
+        }else if(algebricRow === 3){
+            row = 5
+        }else if(algebricRow === 4){
+            row = 4
+        }else if(algebricRow === 5){
+            row = 3
+        }else if(algebricRow === 6){
+            row = 2
         }else if(algebricRow === 7){
             row = 1;
         }else if(algebricRow === 8){
