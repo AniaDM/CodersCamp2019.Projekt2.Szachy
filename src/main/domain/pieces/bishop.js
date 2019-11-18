f4cdd4a4ef9e18c77a76d3fa8243f259d822e575
Bozik71 <32542522+Bozik71@users.noreply.github.com>
2019-11-18 18:51:50 +0100

    #8 implementacja bishop (#16)

--- src/main/domain/pieces/bishop.js
+++ src/main/domain/pieces/bishop.js
@@ -1,4 +1,11 @@
 import Piece from './piece';
+import {
+    PieceMove
+} from "./pieceMove";
+import Side from "./side";
+import {
+    Square
+} from '../board/square';
 
 export default class Bishop extends Piece {
 
@@ -6,4 +13,107 @@ export default class Bishop extends Piece {
         super(side);
     }
 
-}
+    getAvailableMoves(chessBoard, currentSquare) {
+        let normalMoves = [];
+        console.log(`column = ${currentSquare.column.number}, row = ${currentSquare.row.number}`);
+
+        ///////////////////////// 1 FOR  +Y+X   ///////////////////////////
+
+        for (let i = (currentSquare.row.number + 1), j = 1; i < 8; i++, j++) {
+
+            if ((currentSquare.column.number + j) > 7) {
+                break;
+            };
+
+            if (chessBoard.squareIsOccupied(currentSquare.atY(i).atX(currentSquare.column.number + j)) &&
+                !this.canCaptureOnSquare(chessBoard, currentSquare.atY(i).atX(currentSquare.column.number + j))) {
+                break;
+            }
+
+            normalMoves.push(currentSquare.atY(i).atX(currentSquare.column.number + j)); // movie +X
+
+            if (this.canCaptureOnSquare(chessBoard, currentSquare.atY(i).atX(currentSquare.column.number + j))) {
+                break;
+            }
+        };
+
+        ///////////////////////// 2 FOR  -Y+X   ///////////////////////////
+
+        for (let i = currentSquare.row.number - 1, j = 1; i >= 0; i--, j++) {
+
+            if ((currentSquare.column.number + j) > 7) {
+                break;
+            };
+
+            if (chessBoard.squareIsOccupied(currentSquare.atY(i).atX(currentSquare.column.number + j)) &&
+                !this.canCaptureOnSquare(chessBoard, currentSquare.atY(i).atX(currentSquare.column.number + j))) {
+                break;
+            }
+
+            normalMoves.push(currentSquare.atY(i).atX(currentSquare.column.number + j)); // movie +X
+
+            if (this.canCaptureOnSquare(chessBoard, currentSquare.atY(i).atX(currentSquare.column.number + j))) {
+                break;
+            }
+        };
+
+        ///////////////////////// 3 FOR +Y-X    ///////////////////////////
+
+     
+
+        for (let i = (currentSquare.row.number + 1), j = 1; i < 8; i++, j++) {
+
+            if ((currentSquare.column.number - j) < 0) {
+                i++;
+                j++
+         
+                break;
+            };
+
+            if (chessBoard.squareIsOccupied(currentSquare.atY(i).atX(currentSquare.column.number - j)) &&
+                !this.canCaptureOnSquare(chessBoard, currentSquare.atY(i).atX(currentSquare.column.number - j))) {
+                break;
+            }
+
+            normalMoves.push(currentSquare.atY(i).atX(currentSquare.column.number - j)); // movie -X
+
+            if (this.canCaptureOnSquare(chessBoard, currentSquare.atY(i).atX(currentSquare.column.number - j))) {
+                break;
+            }
+        };
+
+
+        ///////////////////////// 4 FOR -Y-X    ///////////////////////////
+
+        for (let i = currentSquare.row.number - 1, j = 1; i >= 0; i--, j++) {
+
+            if ((currentSquare.column.number - j) < 0) {
+                i++;
+                j++
+
+                break;
+            };
+
+            if (chessBoard.squareIsOccupied(currentSquare.atY(i).atX(currentSquare.column.number - j)) &&
+                !this.canCaptureOnSquare(chessBoard, currentSquare.atY(i).atX(currentSquare.column.number - j))) {
+
+                break;
+            }
+
+            normalMoves.push(currentSquare.atY(i).atX(currentSquare.column.number - j)); // movie -X
+
+            if (this.canCaptureOnSquare(chessBoard, currentSquare.atY(i).atX(currentSquare.column.number - j))) {
+      
+                break;
+            }
+
+        };
+
+        normalMoves = normalMoves.map(movableSquare => PieceMove.normalAt(movableSquare));
+        return normalMoves;
+    };
+
+    canCaptureOnSquare(chessBoard, square) {
+        return chessBoard.squareIsOccupied(square) && chessBoard.getPiece(square).isCapturableBy(this);
+    };
+};
