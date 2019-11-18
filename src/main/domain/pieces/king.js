2a174c0b7af33d5d18ae5fbb1b09f7014f0515bc
Marek <marek_sienko@o2.pl>
2019-11-18 03:03:52 +0100

    uzupełnienie pliku king

--- src/main/domain/pieces/king.js
+++ src/main/domain/pieces/king.js
@@ -1,13 +1,31 @@
 import Piece from './piece';
-import Side from './side';
+import {PieceMove} from "./pieceMove";
+import Side from "./side";
+import Square from '../board/square'
 
-export default class King extends Piece {
-
-    constructor(side) {
-        super(side);
-    }
-
-    isCapturableBy(anotherPiece) {
-        return false;
-    }
-}
+export default class Knight extends Piece {    constructor(side) {
+       super(side);
+   }
+   getAvailableMoves(chessBoard, currentSquare) {
+       const deltas = [[2,1],[1,2],[1,-2],[2,-1],[-2,1],[-1,2],[-1,-2],[-2,-1]];
+       let normalMoves=[];
+       let captureMoves=[];
+       for(let delta of deltas){
+           let moveSquare=chessBoard.squareInBounds(currentSquare.column.number+delta[0], currentSquare.row.number + delta[1])
+           ? currentSquare.atY(currentSquare.row.number + delta[1]).atX(currentSquare.column.number+delta[0])
+           : null;
+           if(moveSquare && !this.isMoveToSquareImpossible(chessBoard, moveSquare)){
+                 normalMoves.push(moveSquare);
+           }
+           if(moveSquare && this.canCaptureOnSquare(chessBoard, moveSquare)){
+               captureMoves.push(moveSquare);
+         }        }
+       normalMoves = normalMoves.map(movableSquare => PieceMove.normalAt(movableSquare));
+       captureMoves = captureMoves.map(capturableSquare => PieceMove.captureAt(capturableSquare))
+       return normalMoves
+              .concat(captureMoves);
+   }    canCaptureOnSquare(chessBoard, square) {
+       return chessBoard.squareIsOccupied(square) && chessBoard.getPiece(square).isCapturableBy(this);
+   }    isMoveToSquareImpossible(chessBoard, square) {
+       return !chessBoard.squareInBounds(square.row.number, square.col.number) || chessBoard.squareIsOccupied(square);
+   }}
